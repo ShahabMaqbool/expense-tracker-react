a9e991df3e91f8518fc98e49e51cc355de76b1a6
Shahab Maqbool <numl-f23-58089@numls.edu.pk>
2026-07-17 16:36:32 +0500

    Add income and expense cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpense from "./components/IncomeExpense";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Balance />
+      <IncomeExpense />
     </div>
   );
 }
---
+++ src/components/IncomeExpense.jsx
@@ -0,0 +1,18 @@
+
+function IncomeExpense() {
+  return (
+    <div>
+      <div>
+        <h3>Income</h3>
+        <p>Rs. 0</p>
+      </div>
+
+      <div>
+        <h3>Expense</h3>
+        <p>Rs. 0</p>
+      </div>
+    </div>
+  );
+}
+
+export default IncomeExpense;
